8775428b931bf4f4e7e58b76d6ec21a45b7992cc
pascalzieglerit <pascalzieglerit@gmail.com>
2015-11-25 18:50:38 +0100

    First try to push a skript
    godly hands behavior empty

---
+++ MasterOfTheUniverse/MasterOfTheUniverse/Source/MasterOfTheUniverse/Public/GodlyHandsBehavior.h
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Components/ActorComponent.h"
+#include "GodlyHandsBehavior.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class MASTEROFTHEUNIVERSE_API UGodlyHandsBehavior : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+
+	
+
+	// Sets default values for this component's properties
+	UGodlyHandsBehavior();
+
+	// Called when the game starts
+	virtual void BeginPlay() override;
+	
+	// Called every frame
+	virtual void TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction ) override;
+
+	UFUNCTION(BlueprintCallable, Category = "Grabbing")
+		void onGodlyGrab(IMotionController hand);
+	
+};
